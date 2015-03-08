5a3958668180c13d4927a540f27db97293e2a93c
Tomasz Dziuda <dziudek@gmail.com>
2015-03-08 16:49:24 +0100

    Fixed issues with Twitter output view

--- mod_social_gk5/tmpl/twitterTweets.php
+++ mod_social_gk5/tmpl/twitterTweets.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Gavick Social GK5 - layout file
  * @package Joomla!
@@ -9,9 +8,7 @@
  * @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
  * @version $Revision: GK4 1.0 $
  **/
-
 // no direct access
-
 defined('_JEXEC') or die('Restricted access');
 
 ?>
@@ -19,69 +16,62 @@ defined('_JEXEC') or die('Restricted access');
 <?php if($this->pData == '' || count($this->pData) < 0) : ?>
 	<p><?php echo JText::_( 'MOD_SOCIAL_ANY_DATA' ); ?></p>
 <?php else : ?>
-	<div class="gkTweets">
-		<div>
-		<?php if(($this->config['twitter_rows'] * $this->config['twitter_columns']) > 0 && $this->config['twitter_tweet_amount'] >= ($this->config['twitter_rows'] * $this->config['twitter_columns'])) : ?>
-				
-			<?php 
-				$amount = $this->config['twitter_rows'] * $this->config['twitter_columns']; 
-				if($amount > count($this->pData)) {
-					$amount = count($this->pData);
-				}
-			?>	
-					
-			<?php for($i = 0; $i < $amount; $i++) : ?>
-				
-				<div class="gkTweet" style="width: <?php echo 100/$this->config['twitter_columns']."%!important"; ?>">
-					<div>
-					
-					<?php if($this->config['twitter_show_avatar']) : ?>
-							<img src="<?php echo str_replace('http://', '//', $this->pData[$i]->avatar); ?>" alt="<?php echo $this->pData[$i]->name; ?>" />
-					<?php endif; ?>
-				
-					
-					<?php if($this->config['twitter_show_uname'] || $this->config['twitter_show_fname']) : ?>
-					<span class="gkTweetName">
-							<?php if($this->config['twitter_show_uname']) : ?>
-								<a href="<?php echo 'https://twitter.com/'.$this->pData[$i]->url; ?>"><?php echo $this->pData[$i]->username; ?></a>
-							<?php endif; ?>
-							<?php if($this->config['twitter_show_fname']) : ?>
-								<small><?php echo $this->pData[$i]->name; ?></small>
-							<?php endif; ?>
-					</span>
-					<?php endif; ?>
-					
-					
-					<p class="gkTweetContent"><?php echo $this->pData[$i]->text; ?></p>
-					
-					
-					<span class="gkTweetInfo">
-					<?php if($this->config['twitter_time_mode'] == 'cdate') : ?>
-						<?php echo $this->pData[$i]->time_diff.' '.JText::_( 'MOD_SOCIAL_AGO' ); ?>
-					<?php else : ?>
-						<?php echo $this->pData[$i]->time; ?>
-					<?php endif; ?>
-					
-					<?php if($this->config['twitter_show_actions']) : ?>
-						<a class="reply" href="https://twitter.com/intent/tweet?in_reply_to=<?php echo number_format($this->pData[$i]->id,0,'.',''); ?>"><?php echo JText::_( 'MOD_SOCIAL_REPLY' ) ?></a>
-						<a class="retweet" href="https://twitter.com/intent/retweet?tweet_id=<?php echo number_format($this->pData[$i]->id,0,'.',''); ?>"><?php echo JText::_( 'MOD_SOCIAL_RETWEET' ) ?></a>
-						<a class="favorite" href="https://twitter.com/intent/favorite?tweet_id=<?php echo number_format($this->pData[$i]->id,0,'.',''); ?>"><?php echo JText::_( 'MOD_SOCIAL_FAVORITE' ) ?></a>
-					<?php endif; ?>
-					
-					</span>
-						</div>
-					</div>
-					
-					<?php if(($i+1) % ($this->config['twitter_columns']) == 0) : ?>
-						<div class="gkDivider"></div>
-					<?php endif; ?>
+
+	<?php if(($this->config['twitter_rows'] * $this->config['twitter_columns']) > 0 && $this->config['twitter_tweet_amount'] >= ($this->config['twitter_rows'] * $this->config['twitter_columns'])) : ?>
 			
-			<?php endfor; ?>
+		<?php 
+			$amount = $this->config['twitter_rows'] * $this->config['twitter_columns']; 
+			if($amount > count($this->pData)) {
+				$amount = count($this->pData);
+			}
+		?>
+				
+		<?php for($i = 0; $i < $amount; $i++) : ?>
+		<div class="gkTweet" style="width: <?php echo 100/$this->config['twitter_columns']."%!important"; ?>">
+			<div>
+				<?php if($this->config['twitter_show_avatar']) : ?>
+				<img src="<?php echo str_replace('http://', '//', $this->pData[$i]['avatar']); ?>" alt="<?php echo $this->pData[$i]['name']; ?>" />
+				<?php endif; ?>
 		
 			
-		<?php else : ?>
-			<p><?php echo JText::_( 'MOD_SOCIAL_NOT_SUFFICIENT_TWEETS_F' ).$this->config['twitter_columns']." columns *".$this->config['twitter_rows']. " rows = ".($this->config['twitter_rows'] * $this->config['twitter_columns'])." tweets, ".JText::_( 'MOD_SOCIAL_NOT_SUFFICIENT_TWEETS_S' ).$this->config['twitter_tweet_amount'].JText::_( 'MOD_SOCIAL_NOT_SUFFICIENT_TWEETS_T' ) ; ?></p>
-		<?php endif; ?>
+				<?php if($this->config['twitter_show_uname'] || $this->config['twitter_show_fname']) : ?>
+				<span class="gkTweetName">
+						<?php if($this->config['twitter_show_uname']) : ?>
+							<a href="<?php echo 'https://twitter.com/'.$this->pData[$i]['url']; ?>"><?php echo $this->pData[$i]['username']; ?></a>
+						<?php endif; ?>
+						<?php if($this->config['twitter_show_uname']) : ?>
+							<small><?php echo $this->pData[$i]['name']; ?></small>
+						<?php endif; ?>
+				</span>
+				<?php endif; ?>
+				
+				
+				<p class="gkTweetContent"><?php echo $this->pData[$i]['text']; ?></p>
+				
+				
+				<span class="gkTweetInfo">
+				<?php if($this->config['twitter_time_mode'] == 'cdate') : ?>
+					<?php echo $this->pData[$i]['time_diff'].' '.JText::_( 'MOD_SOCIAL_AGO' ); ?>
+				<?php else : ?>
+					<?php echo $this->pData[$i]['time']; ?>
+				<?php endif; ?>
+				
+				<?php if($this->config['twitter_show_actions']) : ?>
+					<a class="reply" href="https://twitter.com/intent/tweet?in_reply_to=<?php echo $this->pData[$i]['id']; ?>"><?php echo JText::_( 'MOD_SOCIAL_REPLY' ) ?></a>
+					<a class="retweet" href="https://twitter.com/intent/retweet?tweet_id=<?php echo $this->pData[$i]['id']; ?>"><?php echo JText::_( 'MOD_SOCIAL_RETWEET' ) ?></a>
+					<a class="favorite" href="https://twitter.com/intent/favorite?tweet_id=<?php echo $this->pData[$i]['id']; ?>"><?php echo JText::_( 'MOD_SOCIAL_FAVORITE' ) ?></a>
+				<?php endif; ?>
+				
+				</span>
+			</div>
 		</div>
-	</div>
+			
+		<?php if(($i+1) % ($this->config['twitter_columns']) == 0) : ?>
+		<div class="gkDivider"></div>
+		<?php endif; ?>
+		<?php endfor; ?>
+	<?php else : ?>
+		<p><?php echo JText::_( 'MOD_SOCIAL_NOT_SUFFICIENT_TWEETS_F' ).$this->config['twitter_columns']." columns *".$this->config['twitter_rows']. " rows = ".($this->config['twitter_rows'] * $this->config['twitter_columns'])." tweets, ".JText::_( 'MOD_SOCIAL_NOT_SUFFICIENT_TWEETS_S' ).$this->config['twitter_tweet_amount'].JText::_( 'MOD_SOCIAL_NOT_SUFFICIENT_TWEETS_T' ) ; ?></p>
+	<?php endif; ?>
+	
 <?php endif; ?>
